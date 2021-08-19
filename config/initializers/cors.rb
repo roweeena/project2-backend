e656ffb5fffd0ea17c9fb0576a2835e66c8cc337
roweeena <roe0320@gmail.com>
2021-08-19 17:04:04 +1000

    merged and pushed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'http://localhost:3000','http://localhost:3001', 'https://roweeena.github.io', 'https://rpg-generator-backend.herokuapp.com'
 
     resource '*',
-      :headers => :any,
+      :headers => :any, 
       :methods => [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
