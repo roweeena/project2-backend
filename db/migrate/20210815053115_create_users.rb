f5e8e6c03faf4c5047a1a44108cbb1dd885628d1
CDDiaz <cabj_xeneize@hotmail.com>
2021-08-19 16:37:50 +1000

    character creation

--- db/migrate/20210815053115_create_users.rb
+++ db/migrate/20210815053115_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.text :email
-      t.text :name
+      t.text :username
       t.boolean :admin, :default => false
       t.string :password_digest
 
--- db/migrate/20210816072754_add_username_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddUsernameToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :username, :text
-    remove_column :users, :name
-
-  end
-end
