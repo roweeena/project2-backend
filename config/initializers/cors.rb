4536edd233377bd605e84591bd190f8d4aaed8be
roweeena <roe0320@gmail.com>
2021-08-19 13:47:30 +1000

    changed urls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000' #will need to change when deploy
+    origins 'http://localhost:3000','https://roweeena.github.io/', 'https://rpg-generator-backend.herokuapp.com' 
 
     resource '*',
       headers: :any,
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-    Rails.application.config.session_store :cookie_store, key: '_rpg-character-creator', domain: 'your-frontend-domain' # To replace later
+    Rails.application.config.session_store :cookie_store, key: '_rpg-character-creator', domain: 'https://roweeena.github.io/project2/' 
   else
     Rails.application.config.session_store :cookie_store, key: '_rpg-character-creator'
 
