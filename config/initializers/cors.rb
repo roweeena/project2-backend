c5219918466a772a53df72a00868966c0c22a629
roweeena <roe0320@gmail.com>
2021-08-19 14:16:35 +1000

    change to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000','https://roweeena.github.io/', 'https://rpg-generator-backend.herokuapp.com' 
+    origins 'http://localhost:3000','http://localhost:3001', 'https://roweeena.github.io', 'https://rpg-generator-backend.herokuapp.com'
 
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      :headers => :any,
+      :methods => [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
 end
